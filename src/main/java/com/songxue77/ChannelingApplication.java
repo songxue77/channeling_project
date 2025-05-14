e13fd02b438e8439fe10a08a34f80275c2f6fe1d
심설송 <snowbrightly@hotmail.com>
2025-05-15 00:45:09 +0900

    CHN-1 Add project init file

---
+++ src/main/java/com/songxue77/ChannelingApplication.java
@@ -0,0 +1,13 @@
+package com.songxue77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songxue77/channeling/ChannelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songxue77.channeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
