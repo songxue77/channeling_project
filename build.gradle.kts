411867feffa05684e70fac7372ac979012734c75
심설송 <snowbrightly@hotmail.com>
2025-06-01 01:49:50 +0900

    CHN-4 Add spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version ("3.4.5")
 	id("io.spring.dependency-management") version ("1.1.7")
+	id("com.diffplug.spotless") version ("7.0.4")
 }
 
 group = "com.songxue77"
@@ -32,6 +33,52 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+spotless {
+	val excludeFiles = arrayOf("./idea/**/*.*", ".vscode/**/*.*")
+
+	java {
+		targetExclude("build/**/*.java")
+		removeUnusedImports()
+		replaceRegex("Remove wildcard imports", "import\\s+[^\\*\\s]+\\*;(\\r\\n|\\r|\\n)", "$1")
+		replaceRegex(
+			"Replace lombok.NonNull with spring NonNull",
+			"import lombok.NonNull",
+			"import org.springframework.lang.NonNull;"
+		)
+		googleJavaFormat()
+		importOrder(
+			"java",
+			"jakarta",
+			"javax",
+			"lombok",
+			"org.springframework",
+			"",
+			"org.junit",
+			"com.songxue77",
+			"\\#",
+			"\\#org.junit",
+			"\\#com.songxue77"
+		)
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
+	format("yaml") {
+		target("**/*.yaml", "**/*.yml")
+		targetExclude(*excludeFiles)
+		prettier().configFile("$rootDir/.prettierrc")
+	}
+	format("xml") {
+		target("**/*.xml")
+		targetExclude(*excludeFiles)
+		prettier().config(mapOf("parser" to "html", "printWidth" to 160)).configFile("$rootDir/.prettierrc")
+	}
+	format("md") {
+		target("**/*.md")
+		targetExclude(*excludeFiles)
+		prettier().config(mapOf("parser" to "markdown", "printWidth" to 160)).configFile("$rootDir/.prettierrc")
+	}
 }
 
 tasks.test {
